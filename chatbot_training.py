2757894f9f149ea4b9f3f0fece3e89e2f2c4f47a
Trần Trung Thành <thanhtt.amg@gmail.com>
2021-06-04 23:21:13 +0700

    Commit chatbot voice recognition

--- chatbot_training.py
+++ chatbot_training.py
@@ -71,9 +71,9 @@ net = tflearn.regression(net)
 model = tflearn.DNN(net)
 
 
-model.load("chatbot/data/model.tflearn")
-# model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
-# model.save("data/model.tflearn")
+# model.load("chatbot/data/model.tflearn")
+model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+model.save("data/model.tflearn")
 
 
 def bag_of_words(s, words):
